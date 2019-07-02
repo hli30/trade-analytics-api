5706dc73e1e81e037415db118edbe007abeea960
Harry L <harrylll93@gmail.com>
2019-07-02 09:52:08 -0700

    initial commit

---
+++ trade_history/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Trade_History(models.Model):
+	EQUITY = 'EQ'
+	ASSET_TYPE_CHOICES = [
+		(EQUITY, 'Equity')
+	]
+
+	BUY = 'B'
+	SELL = 'S'
+	ACTION_CHOICES = [
+		(BUY, 'Buy'),
+		(SELL, 'Sell')
+	]
+
+	user_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
+
+	asset_type = models.CharField(max_length=2, choices=ASSET_TYPE_CHOICES)
+	symbol = models.CharField(max_length=10)
+	description = models.CharField(max_length=100, blank=True)
+	action = models.CharField(max_length=1, choices=ACTION_CHOICES)
+	quantity = models.IntegerField()
+	price = models.DecimalField(max_digits=10 ,decimal_places=2)
+	commission = models.DecimalField(max_digits=6 ,decimal_places=2)
+
+
+
+
+
+
