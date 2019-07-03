848560e0ec81e9945198ad9bb3fa6763c4014421
Harry L <harrylll93@gmail.com>
2019-07-03 08:00:40 -0700

    changed user model name to account and registered models to admin

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
+class Account(AbstractUser):
 	email = models.EmailField(unique=True)
 	username = models.CharField(max_length=15, blank=True, null=True)
 
